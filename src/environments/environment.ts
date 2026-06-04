e126cff636af434dcdd49e9f594671e3f73351ee
Tu Nombre <tuemail@ejemplo.com>
2026-06-04 16:17:46 +0200

    enviroment en produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://urbanwear-backend-q1yy.onrender.com',
   cloudinary: {
-    cloudName: 'dv7aunoql',       
-    uploadPreset: 'urbanwear_preset'   
+    cloudName: 'dv7aunoql',
+    uploadPreset: 'urbanwear_preset'
   }
 };
