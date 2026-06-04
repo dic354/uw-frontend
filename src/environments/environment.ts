48b9c35018fa4b97088e69e69abf8635dc2d7d6b
dic354 <91329498+dic354@users.noreply.github.com>
2026-06-04 18:01:20 +0200

    Remove trailing slash from apiUrl in environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiUrl: window.location.hostname === 'localhost'
     ? 'http://localhost:3000/'
-    : 'https://urbanwear-backend-q1yy.onrender.com/',
+    : 'https://urbanwear-backend-q1yy.onrender.com',
   cloudinary: {
     cloudName: 'dv7aunoql',
     uploadPreset: 'urbanwear_preset'
