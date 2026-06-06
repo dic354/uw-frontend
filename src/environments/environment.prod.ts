093f9eca38390b794e3aee91fc56b794e6edc791
David <davidigco7299@gmail.com>
2026-06-06 17:16:50 +0200

    vercel + environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://urbanwear-backend-q1yy.onrender.com',
+  apiUrl: 'https://uw-backend-ozmo.onrender.com',
   cloudinary: {
     cloudName: 'dv7aunoql',
     uploadPreset: 'urbanwear_preset'
